7e576e13b7209e290c3d367959a7c24d6ec450e4
João Carlos Chavatte <chavatte@hotmail.com>
2021-09-04 10:30:22 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,15 +10,7 @@ function ensureAutenticated(request, response, next) {
     const authHeader = request.headers.authorization;
     if (!authHeader) {
         return response.status(403).json({ message:"[CONTÉUDO EDUCACIONAL] PROJETO FINAL DO CURSO DE DESENVOLVIMENTO DE SISTEMAS DO SENAC SÃO CARLOS"
- "             _____ __  __________ ______   ____________ _____ __  _____    __       ________   ____ ______  __ __  ___________ "
-	" ___     / ___// / / /  _/ __ \_  __/  / ____/ ____ \ ___// / / /   |  / /      / ____/ /  / __ \_  __/_/ // /_/ ____/ ___/    "
-	" / _ \    \__ \/ /_/ // // /_/ // /    / /   / / __ `\__ \/ / / / /| | / /      / /   / /  / / / // / /_  _  __/ __/  \__ \    "
-	"/  __/   ___/ / __  // // _, _// /    / /___/ / /_/ /__/ / /_/ / ___ |/ /___   / /___/ /___ /_/ // / /_  _  __/ /___ ___/ /    " 
-	"\___/   /____/_/ /_/___/_/ |_|/_/     \____/\ \__,_/____/\____/_/  |_/_____/   \____/_____\____//_/   /_//_/ /_____//____/     "
-                                      
-
-            
-            
+  
         });
     }
 
