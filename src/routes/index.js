29172e8fed0fc083edd6a1f4399b4553a28a9f43
João Carlos Chavatte <chavatte@hotmail.com>
2021-09-04 10:21:44 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -9,37 +9,7 @@ const routes = express.Router();
 function ensureAutenticated(request, response, next) {
     const authHeader = request.headers.authorization;
     if (!authHeader) {
-        return response.status(403).json({ message:
-                                          
-"             _____ __  __________ ______   ____________ _____ __  _____    __       ________   ____ ______  __ __  ___________ "
-" ___     / ___// / / /  _/ __ \_  __/  / ____/ ____ \ ___// / / /   |  / /      / ____/ /  / __ \_  __/_/ // /_/ ____/ ___/    "
-" / _ \    \__ \/ /_/ // // /_/ // /    / /   / / __ `\__ \/ / / / /| | / /      / /   / /  / / / // / /_  _  __/ __/  \__ \    "
-"/  __/   ___/ / __  // // _, _// /    / /___/ / /_/ /__/ / /_/ / ___ |/ /___   / /___/ /___ /_/ // / /_  _  __/ /___ ___/ /    " 
-"\___/   /____/_/ /_/___/_/ |_|/_/     \____/\ \__,_/____/\____/_/  |_/_____/   \____/_____\____//_/   /_//_/ /_____//____/     "
-"                                                                                                                               "
-                                                                                                          
-           "[CONTÉUDO EDUCACIONAL] PROJETO FINAL DO CURSO DE DESENVOLVIMENTO DE SISTEMAS DO SENAC SÃO CARLOS"
-                                                                                                          
-                                         "SEJA DIFERENTE, SEJA VOCÊ MESMO!"
-            
-            "Autores:"
-
-                    "João Carlos Chavatte – joao.cchavatte@senacsp.edu.br"
-
-                    "Michael Dantas Siqueira dos Santos – michael.dssantos1@senacsp.edu.br"
-
-                    "Robinson Queiroz Machado – robinson.qmachado@senacsp.edu.br"
-            
-            
-            
-"Repositório no Github: https://github.com/e-shirt-casual-clothes/"
-
-"Interface gráfica do utilizador (GUI - Graphical User Interface): https://loja.e-shirt-casual-clothes.store/"
-        
-"Projeto: https://e-shirt-casual-clothes.store/"
-        
-        
-                                                                    "Dúvidas, críticas ou sugestões: contato@e-shirt-casual-clothes.store"
+        return response.status(403).json({ message:"[CONTÉUDO EDUCACIONAL] PROJETO FINAL DO CURSO DE DESENVOLVIMENTO DE SISTEMAS DO SENAC SÃO CARLOS"
 
             
             
